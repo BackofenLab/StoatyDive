5ae8cff4cebfa2a22075a8e875a1b1610862ccf5
Florian-H-Lab <f.heyl@gmx.de>
2019-06-04 15:37:35 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup
 
-setup(name='StoatyDive', version='1.0.1', packages=['lib'])
+setup(name='StoatyDive',
+      author='Florian Heyl',
+      description='StoatyDive is a tool to evaluate predicted peak profiles to assess the binding specificity of a protein to its targets.',
+      author_email='heylf@informatik.uni-freiburg.de',
+      url='https://github.com/heylf/StoatyDive',
+      license="GNU General Public License v3.0  (GPL)",
+      version='1.0.2',
+      packages=['lib'],
+      scripts=['bin/StoatyDive.py'])
