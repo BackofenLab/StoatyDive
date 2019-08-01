ebb3964acb0fd42b3dc45802ec8fa63ced2a777c
flo.minion.info@gmail.com <flo.minion.info@gmail.com>
2019-08-01 17:22:31 +0200

    Changes to setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='StoatyDive',
       author='Florian Heyl',
-      description='StoatyDive is a tool to evaluate predicted peak profiles to assess the binding specificity of a protein to its targets.',
+      description='StoatyDive is a tool to evaluate and classify predicted peak profiles to assess the binding specificity of a protein to its targets.',
       author_email='heylf@informatik.uni-freiburg.de',
       url='https://github.com/heylf/StoatyDive',
       license="GNU General Public License v3.0  (GPL)",
